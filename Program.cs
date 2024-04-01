24dd7bcbbfeba8a65306f34709b8965db4becf94
Egordimov <80635732+Egordimov@users.noreply.github.com>
2024-04-01 13:25:53 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using System.Net.NetworkInformation;
+
+namespace HelloWorld
+{
+    class Hello
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world from Dimok");
+        }
+    }
+}
