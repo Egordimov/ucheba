d950f320cb82bb159459632112138b2f430f0f13
Egordimov <80635732+Egordimov@users.noreply.github.com>
2024-04-01 13:22:35 +0300

    Add files via upload

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter the first number : ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number : ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the third number : ");
+            int num3 = int.Parse(Console.ReadLine());
+            if (num1 > num2)
+            {
+                if (num1 > num3)
+                {
+                    Console.WriteLine("largest number : " + num1);
+                }
+                else
+                {
+                    Console.WriteLine("largest number : " + num3);
+                }
+            }
+            else
+            {
+                if (num2 > num3)
+                {
+                    Console.WriteLine("largest number : " + num2);
+                }
+                else
+                {
+                    Console.WriteLine("largest number : " + num3);
+                }
+
+            }
+        }
+    }    
+}
